e144e7e6eb61af6e349be60a6a3afb21b4cdb613
khoadng <nguyenduckhoa1996@gmail.com>
2025-06-25 19:02:37 +0700

    chore: remove web

--- flutter_avif/lib/flutter_avif.dart
+++ flutter_avif/lib/flutter_avif.dart
@@ -2,6 +2,4 @@ library flutter_avif;
 
 export 'src/avif_image.dart';
 export 'src/avif_encoder.dart';
-export 'src/avif_animation.dart';
 export 'src/avif_decoder.dart';
-export 'src/cached_network_avif_image.dart';
--- flutter_avif/lib/src/avif_encoder.dart
+++ flutter_avif/lib/src/avif_encoder.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
@@ -22,69 +21,36 @@ Future<Uint8List> encodeAvif(
   int averageFps = 0, width = 0, height = 0;
 
   Uint8List exifData = Uint8List(0);
-  int orientation = 1;
   if (keepExif) {
     final decodedExif = await exif.readExifFromBytes(input);
     exifData = encodeExif(decodedExif);
-    orientation = decodedExif['Image Orientation']?.values.toList()[0] ??
-        decodedExif['EXIF Orientation']?.values.toList()[0] ??
-        decodedExif['Thumbnail Orientation']?.values.toList()[0] ??
-        decodedExif['Interoperability Orientation']?.values.toList()[0] ??
-        1;
   }
 
-  if (kIsWeb) {
-    final decoded =
-        await avif_platform.FlutterAvifPlatform.decode(input, orientation);
-    int totalDurationMs = 0;
-    int frameSize = decoded.width * decoded.height * 4;
+  final decoder = await ui.instantiateImageCodec(input);
+  final List<ui.FrameInfo> frames = [];
+  int totalDurationMs = 0;
+  for (int i = 0; i < decoder.frameCount; i += 1) {
+    final frame = await decoder.getNextFrame();
+    totalDurationMs += frame.duration.inMilliseconds;
+    frames.add(frame);
+  }
 
-    width = decoded.width;
-    height = decoded.height;
+  width = frames[0].image.width;
+  height = frames[0].image.height;
+  averageFps = decoder.frameCount > 1 && totalDurationMs > 0
+      ? (1000 * decoder.frameCount / totalDurationMs).round()
+      : 1;
+  final timebaseMs = (1000 / averageFps).round();
 
-    for (int i = 0; i < decoded.durations.length; i += 1) {
-      totalDurationMs += decoded.durations[i];
-    }
-
-    averageFps = decoded.durations.length > 1 && totalDurationMs > 0
-        ? (1000 * decoded.durations.length / totalDurationMs).round()
-        : 1;
-    final timebaseMs = (1000 / averageFps).round();
-
-    for (int i = 0; i < decoded.durations.length; i += 1) {
-      final frame = decoded.data.sublist(i * frameSize, (i + 1) * frameSize);
+  for (int i = 0; i < frames.length; i += 1) {
+    final imageData =
+        await frames[i].image.toByteData(format: ui.ImageByteFormat.rawRgba);
+    if (imageData != null) {
       encodeFrames.add(avif_platform.EncodeFrame(
-        data: frame,
-        durationInTimescale: (decoded.durations[i] / timebaseMs).round(),
+        data: imageData.buffer.asUint8List(),
+        durationInTimescale:
+            (frames[i].duration.inMilliseconds / timebaseMs).round(),
       ));
-    }
-  } else {
-    final decoder = await ui.instantiateImageCodec(input);
-    final List<ui.FrameInfo> frames = [];
-    int totalDurationMs = 0;
-    for (int i = 0; i < decoder.frameCount; i += 1) {
-      final frame = await decoder.getNextFrame();
-      totalDurationMs += frame.duration.inMilliseconds;
-      frames.add(frame);
-    }
-
-    width = frames[0].image.width;
-    height = frames[0].image.height;
-    averageFps = decoder.frameCount > 1 && totalDurationMs > 0
-        ? (1000 * decoder.frameCount / totalDurationMs).round()
-        : 1;
-    final timebaseMs = (1000 / averageFps).round();
-
-    for (int i = 0; i < frames.length; i += 1) {
-      final imageData =
-          await frames[i].image.toByteData(format: ui.ImageByteFormat.rawRgba);
-      if (imageData != null) {
-        encodeFrames.add(avif_platform.EncodeFrame(
-          data: imageData.buffer.asUint8List(),
-          durationInTimescale:
-              (frames[i].duration.inMilliseconds / timebaseMs).round(),
-        ));
-      }
     }
   }
 
